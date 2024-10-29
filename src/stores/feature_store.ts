96f96aba8d38a1b07ec5a4011b225c7a869c4bf7
akleinau <56925445+akleinau@users.noreply.github.com>
2024-10-29 13:00:07 +0100

    improve distributions

--- src/stores/feature_store.ts
+++ src/stores/feature_store.ts
@@ -1,16 +1,26 @@
 import {defineStore} from "pinia";
 
 export interface bin {
-    min: number,
-    max: number,
     count: number,
 }
+
+export interface bin_continuous extends bin {
+    min: number,
+    max: number,
+}
+
+export interface bin_discrete extends bin {
+    value: number,
+}
+
+const max_discrete_bins = 24
 
 export const useFeatureStore = defineStore({
     id: 'feature',
     state: () => ({
         feature_names: [] as string[],
         feature_bins: {} as { [key: string]: bin[] },
+        feature_types: {} as { [key: string]: string },
     }),
     actions: {
 
@@ -23,29 +33,73 @@ export const useFeatureStore = defineStore({
             this.feature_bins = {}
             for (let feature of this.feature_names) {
                 const values = data.map((d) => d[feature])
-                const unique_values = Array.from(new Set(values))
-                const bin_number = unique_values.length < 10 ? unique_values.length : 10
-                const min = Math.min(...values)
-                const max = Math.max(...values)
-                const bin_size = (max - min) / bin_number
-                const bins = Array.from({length: bin_number+1}, (_, i) => {
-                    return {
-                        min: min + i * bin_size,
-                        max: min + (i + 1) * bin_size,
-                        count: 0
+                const unique_values = Array.from(new Set(values)).sort()
+
+                //continuous
+                if (unique_values.length > max_discrete_bins) {
+                    this.feature_types[feature] = "continuous"
+                    const bin_number = 20
+                    const min = Math.min(...values)
+                    const max = Math.max(...values)
+                    const bin_size = (max - min) / bin_number
+                    const bins = Array.from({length: bin_number}, (_, i) => {
+                        const i_min = min + i * bin_size
+                        let i_max = i_min + bin_size
+                        if (i === bin_number - 1) {
+                            i_max = i_max + 0.0001 //makes sure the max value is included in the last bin
+                        }
+                        return {
+                            min: i_min,
+                            max: i_max,
+                            count: 0
+                        }
+                    })
+                    for (let value of values) {
+                        let bin_index = Math.floor((value - min) / bin_size)
+                        if (bin_index === bin_number) {
+                            bin_index = bin_number - 1
+                        }
+                        bins[bin_index].count += 1
                     }
-                })
-                for (let value of values) {
-                    const bin_index = Math.floor((value - min) / bin_size)
-                    bins[bin_index].count += 1
+
+                    this.feature_bins[feature] = bins
                 }
-                this.feature_bins[feature] = bins
+
+                //categorical/ discrete
+                else {
+                    this.feature_types[feature] = "discrete"
+                    const bins = unique_values.map((value) => {
+                        return {
+                            value: value,
+                            count: 0
+                        }
+                    })
+
+                    for (let value of values) {
+                        const bin_index = bins.findIndex((bin) => bin.value === value)
+                        bins[bin_index].count += 1
+                    }
+
+                    this.feature_bins[feature] = bins
+                }
+
+
             }
         },
 
         get_feature_bins(feature: string): bin[] {
             return this.feature_bins[feature]
         },
+
+        get_instance_bin_index(feature: string, value: number): number {
+            if (this.feature_types[feature] === "continuous") {
+                const bins = this.feature_bins[feature] as bin_continuous[]
+                return bins.findIndex((bin) => bin.min <= value && bin.max > value)
+            } else {
+                const bins = this.feature_bins[feature] as bin_discrete[]
+                return bins.findIndex((bin) => bin.value === value)
+            }
+        }
 
     }
 });
--- src/visualizations/distribution-vis.vue
+++ src/visualizations/distribution-vis.vue
@@ -20,46 +20,96 @@ onMounted(() => {
 const update_vis = () => {
   const bins = featureStore.get_feature_bins(props.feature_name)
   const instance_value = dataStore.instance[props.feature_name]
+  const instance_bin_index = featureStore.get_instance_bin_index(props.feature_name, instance_value)
 
   let svg = d3.create("svg")
-      .attr("width", 1000)
-      .attr("height", 30)
+      .attr("width", 1020)
+      .attr("height", 60)
+      .attr("viewBox",[-10, 0, 1020, 60])
 
   // add a distribution heatmap over the counts of the bins
   const max_count = d3.max(bins.map(d => d.count))
+  const feature_min = d3.min(bins.map(d => d.min == undefined ? d.value : d.min))
+  const feature_max = d3.max(bins.map(d => d.max == undefined ? d.value : d.max))
   const color = d3.scaleLinear<string>()
       .domain([0, max_count])
       .range(["white", "navy"])
-  const x = d3.scaleLinear()
-      .domain([0, bins.length])
-      .range([0, 1000])
 
-  //add rectangles
   const rect_width = 1000 / bins.length
   const rect_height = 30
-  svg.selectAll("rect")
+
+  const x = d3.scaleLinear()
+      .domain([feature_min, feature_max])
+      .range([rect_width/2, 1000-rect_width/2])
+
+
+  //add rectangles
+  let bin_elements = svg.selectAll(".bin_elements")
       .data(bins)
       .enter()
+      .append("g")
+      .attr("class", "bin_elements")
+        .attr("x", d => d.min == undefined ? x(d.value) - rect_width/2 : x(d.min))
+      .attr("y", d => 5)
+      .attr("width", rect_width)
+      .attr("height", rect_height)
+  bin_elements
       .append("rect")
-      .attr("x", (d, i) => x(i))
-      .attr("y", d => 0)
+      .attr("x", d => d.min == undefined ? x(d.value) - rect_width/2 : x(d.min))
+      .attr("y", d => 5)
       .attr("width", rect_width)
       .attr("height", rect_height)
       .attr("fill", d => color(d.count))
-      .attr("stroke", d => d.min <= instance_value && d.max > instance_value ? "skyblue" : "none")
-      .attr("stroke-width", 8)
-
-  //add text
-  svg.selectAll("text")
-      .data(bins)
-      .enter()
+      .attr("stroke", "white")
+      .attr("stroke-width", 1)
+  bin_elements
       .append("text")
-      .attr("x", (d, i) => x(i) + rect_width/2)
+      .text(d => d.count)
+      .attr("x", d => x(d.min == undefined ? d.value : d.min))
       .attr("y", rect_height/2)
+      .attr("dy", "0.4em")
       .attr("text-anchor", "middle")
       .attr("alignment-baseline", "middle")
       .attr("fill", d => d.count > max_count/2 ? "white" : "black")
-      .text(d => d.min.toFixed(2))
+      .style("opacity", "0")
+      .text(d => d.count)
+
+  bin_elements
+      .on("mouseover", (event, d) => {
+        console.log(event)
+        d3.select(event.target.parentNode).selectAll("text")
+            .style("opacity", "1")
+      })
+      .on("mouseout", (event, d) => {
+        d3.select(event.target.parentNode).selectAll("text")
+            .style("opacity", "0")
+      })
+
+  // add x-axis with padding
+  const xAxis = d3.axisBottom(x)
+
+  if (bins.length < 4) {
+    xAxis.tickValues(bins.map(d => d.value))
+  }
+  else {
+    xAxis.tickValues([feature_min, feature_max])
+  }
+
+  let axis = svg.append("g")
+      .attr("transform", "translate(0, " + (rect_height +5) + ")")
+      .style("font-size", "12px")
+      .call(xAxis)
+  axis.selectAll(".domain") //remove the axis line
+      .remove()
+
+  // add a line for the instance value
+  svg.append("line")
+      .attr("x1", x(instance_value))
+      .attr("y1", 0)
+      .attr("x2", x(instance_value))
+      .attr("y2", rect_height+10)
+      .attr("stroke", "red")
+      .attr("stroke-width", 4)
 
 
   d3.select(container.value).selectAll("*").remove()
