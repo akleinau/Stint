4b53e20e50618d864617f13c65089a5eeb22be85
akleinau <56925445+akleinau@users.noreply.github.com>
2025-02-07 18:31:17 +0100

    add icon

--- src/App.vue
+++ src/App.vue
@@ -52,20 +52,29 @@ const explain = async () => {
   <div id="app">
     <v-card class="mt-5 h-100 mb-2">
       <v-card-item>
-        <v-card-title class="d-flex flex-column align-center">
-          <h1 class="mt-5">STINT - Stories about Interactions</h1>
+        <v-card-title class="d-flex flex-column align-center mb-0">
+          <div class="d-flex flex-column align-center justify-end">
+            <h1 class="mt-5">STINT - Stories about Interactions</h1>
+
+          </div>
         </v-card-title>
+
+        <div class="d-flex flex-column align-center story_text px-10 mt-0" style="text-align:center">
+          <i>
+
+            STINT shows you interactions in your data set relevant for predictions, <br>
+            <span
+                style="color:#6a6c75"> eg. the interplay of hour and weekday when predicting bike rentals. </span>
+            <br>
+            Use it before model training to understand your data or after training to understand your models'
+            predictions.
+          </i>
+
+          <img src="../public/stint1.png" alt="STINT" style="height: 40px" class="ma-4"/>
+        </div>
       </v-card-item>
       <v-card-text>
 
-        <div class="d-flex flex-column align-center mb-5 story_text px-10" style="text-align:center">
-          <i>
-
-            STINT shows you interactions in your data set relevant for predicting a target, <br>
-            <span style="color:#6a6c75"> eg. how the hour and weekday play together when predicting bike rentals. </span> <br>
-            Use it before model training to understand your data set or after model training to understand your models' predictions.
-          </i>
-        </div>
 
         <DataInput/>
 
@@ -95,7 +104,8 @@ const explain = async () => {
       </v-card-text>
 
       <v-card-actions class="bg-grey-lighten-2 justify-center">
-          <v-btn class="w-100" @click="dataStore.selected_feature = null" variant="text" prepend-icon="mdi-close">close</v-btn>
+        <v-btn class="w-100" @click="dataStore.selected_feature = null" variant="text" prepend-icon="mdi-close">close
+        </v-btn>
       </v-card-actions>
     </v-card>
 
@@ -108,7 +118,7 @@ const explain = async () => {
       </v-card-text>
     </v-card>
 
-      <v-card class="h-100 detailCard" v-if="dataStore.storyIsVisible && detailStore.selected_feature !== null">
+    <v-card class="h-100 detailCard" v-if="dataStore.storyIsVisible && detailStore.selected_feature !== null">
 
 
       <v-card-text>
@@ -118,12 +128,14 @@ const explain = async () => {
       </v-card-text>
 
       <v-card-actions class="bg-grey-lighten-2 justify-center">
-          <v-btn class="w-100" @click="detailStore.selected_feature = null" variant="text" prepend-icon="mdi-close">close</v-btn>
+        <v-btn class="w-100" @click="detailStore.selected_feature = null" variant="text" prepend-icon="mdi-close">
+          close
+        </v-btn>
       </v-card-actions>
 
     </v-card>
 
-      <div v-if="dataStore.interacting_features.length !== 0"
+    <div v-if="dataStore.interacting_features.length !== 0"
          class="mt-1 mb-3 d-flex flex-column align-center pt-4 pb-4">
       <OpenEnded/>
     </div>
@@ -143,17 +155,17 @@ html, body {
   margin: 0;
   padding: 0;
   background-color: #384f54;
-  font-size:16px;
+  font-size: 16px;
 }
 
 .v-card {
-  margin:auto !important;
-  width:900px !important;
-  font-size:16px;
+  margin: auto !important;
+  width: 900px !important;
+  font-size: 16px;
 }
 
 .story_text {
-  font-size:16px;
+  font-size: 16px;
 }
 
 .highlight {
@@ -163,9 +175,8 @@ html, body {
 }
 
 
-
 h3 {
-  font-size:18px
+  font-size: 18px
 }
 
 </style>
